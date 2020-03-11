3af898015e6f2020755c4ccda548d823adfb448d
Jenkins user <jenkins@ip-10-35-65-134.eu-west-2.compute.internal>
2020-03-11 16:15:42 +0000

    Creating new service tracking-consent-frontend

---
+++ app/uk/gov/hmrc/trackingconsentfrontend/controllers/HelloWorldController.scala
@@ -0,0 +1,22 @@
+package uk.gov.hmrc.trackingconsentfrontend.controllers
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import uk.gov.hmrc.trackingconsentfrontend.config.AppConfig
+import uk.gov.hmrc.trackingconsentfrontend.views.html.hello_world
+
+import scala.concurrent.Future
+
+@Singleton
+class HelloWorldController @Inject()(appConfig: AppConfig, mcc: MessagesControllerComponents)
+    extends FrontendController(mcc) {
+
+  implicit val config: AppConfig = appConfig
+
+  val helloWorld: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(hello_world()))
+  }
+
+}
