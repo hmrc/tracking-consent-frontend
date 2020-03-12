bc216eaa5cdfd909e258866f8007a5abb3fc1b0b
Matthew Mascord <27346863+matthewmascord@users.noreply.github.com>
2020-03-12 14:46:16 +0000

    Add basic banner functionality

--- app/uk/gov/hmrc/trackingconsentfrontend/assets/src/render.ts
+++
@@ -1,7 +0,0 @@
-const render = () => {
-  const text = document.createTextNode('Hello World')
-  const parentNode = document.body
-  parentNode.insertBefore(text, parentNode.firstChild)
-}
-
-export default render
