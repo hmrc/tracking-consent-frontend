c49f0352dd4f9937f166237254ee115feb96143f
Nat Carey <nat@dorightdigital.com>
2020-03-17 08:48:44 +0000

    Hooked up render to preference storage.

--- app/uk/gov/hmrc/trackingconsentfrontend/assets/src/ui/renderBanner.ts
+++ app/uk/gov/hmrc/trackingconsentfrontend/assets/src/ui/renderBanner.ts
@@ -1,10 +1,16 @@
 // @ts-ignore
 import bannerHtml from './banner.html';
 
-const renderBanner = () => {
+const renderBanner = (userPreference) => {
     const banner = document.createElement('div')
     banner.className = banner.className + ' cookie-banner';
     banner.innerHTML = bannerHtml
+    banner.querySelectorAll('.acceptAll').forEach(acceptAllButton => {
+        (acceptAllButton).addEventListener('click', e => {
+            e.preventDefault()
+            userPreference.userAcceptsAll()
+        })
+    })
     const parentNode = document.body
     parentNode.insertBefore(banner, parentNode.firstChild)
 }
