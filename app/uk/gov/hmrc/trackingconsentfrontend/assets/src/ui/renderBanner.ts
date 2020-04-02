525fc791f4cef0ff8fcff1f8753b7999f3a7eb10
Matthew Mascord <27346863+matthewmascord@users.noreply.github.com>
2020-04-02 16:16:05 +0100

    Use document passed in

--- app/uk/gov/hmrc/trackingconsentfrontend/assets/src/ui/renderBanner.ts
+++ app/uk/gov/hmrc/trackingconsentfrontend/assets/src/ui/renderBanner.ts
@@ -10,48 +10,48 @@ import {
 import removeElement from '../common/removeElement';
 import callIfNotNull from '../common/callIfNotNull';
 import {UserPreferences} from "../../types/UserPreferences";
-import withContentLoaded from "../common/withContentLoaded";
 
-const handleAcceptAllClick = (userPreference: UserPreferences) => (event: Event) => {
-    event.preventDefault()
+export const renderBanner = (document, userPreference: UserPreferences) => {
 
-    userPreference.userAcceptsAll()
-    renderConfirmationMessage()
-    hideQuestion()
-}
+    const handleAcceptAllClick = (userPreference: UserPreferences) => (event: Event) => {
+        event.preventDefault()
 
-const hideQuestion = () => {
-    const question = document.querySelector(`.${COOKIE_BANNER_QUESTION_CLASS}`)
-    callIfNotNull(question, element => removeElement(question))
-}
-
-const insertAtTopOfBody = (banner: HTMLElement) => {
-    const parentNode = document.body
-    parentNode.insertBefore(banner, parentNode.firstChild)
-}
-
-const insertAfterSkipLink = (banner: HTMLElement) => {
-    const skipLink = document.querySelector(`#${SKIP_LINK_CONTAINER_ID}, .${GOV_UK_SKIP_LINK_CLASS}`)
-    if (skipLink !== null) {
-        skipLink.insertAdjacentElement('afterend', banner)
-    } else {
-        insertAtTopOfBody(banner)
+        userPreference.userAcceptsAll()
+        renderConfirmationMessage()
+        hideQuestion()
     }
-}
 
-const insertBanner = (userPreference: UserPreferences) => {
-    const banner = document.createElement('div')
-    banner.className = COOKIE_BANNER_CLASS;
-    banner.innerHTML = bannerHtml
-    banner.setAttribute('role', 'region')
-    banner.setAttribute('aria-label', 'Cookie Banner')
-    const acceptAllButton = banner.querySelector(`.${ACCEPT_ALL_CLASS}`)
-    callIfNotNull(acceptAllButton, element => element.addEventListener('click', handleAcceptAllClick(userPreference)))
+    const hideQuestion = () => {
+        const question = document.querySelector(`.${COOKIE_BANNER_QUESTION_CLASS}`)
+        callIfNotNull(question, element => removeElement(question))
+    }
 
-    insertAfterSkipLink(banner)
-}
+    const insertAtTopOfBody = (banner: HTMLElement) => {
+        const parentNode = document.body
+        parentNode.insertBefore(banner, parentNode.firstChild)
+    }
 
-export const renderBanner = (_, userPreference: UserPreferences) => {
+    const insertAfterSkipLink = (banner: HTMLElement) => {
+        const skipLink = document.querySelector(`#${SKIP_LINK_CONTAINER_ID}, .${GOV_UK_SKIP_LINK_CLASS}`)
+        if (skipLink !== null) {
+            skipLink.insertAdjacentElement('afterend', banner)
+        } else {
+            insertAtTopOfBody(banner)
+        }
+    }
+
+    const insertBanner = (userPreference: UserPreferences) => {
+        const banner = document.createElement('div')
+        banner.className = COOKIE_BANNER_CLASS;
+        banner.innerHTML = bannerHtml
+        banner.setAttribute('role', 'region')
+        banner.setAttribute('aria-label', 'Cookie Banner')
+        const acceptAllButton = banner.querySelector(`.${ACCEPT_ALL_CLASS}`)
+        callIfNotNull(acceptAllButton, element => element.addEventListener('click', handleAcceptAllClick(userPreference)))
+
+        insertAfterSkipLink(banner)
+    }
+
     if (!userPreference.getUserHasSavedCookiePreferences()) {
         insertBanner(userPreference)
     }
