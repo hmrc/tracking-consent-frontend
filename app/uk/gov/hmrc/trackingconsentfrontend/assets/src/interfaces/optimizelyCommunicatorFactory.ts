9bd3734d96b461aea7344de870373b038ba6264b
Nat Carey <nat@dorightdigital.com>
2020-10-08 10:21:32 +0100

    Control optimizely via tracking.js

--- app/uk/gov/hmrc/trackingconsentfrontend/assets/src/interfaces/optimizelyCommunicatorFactory.ts
+++ app/uk/gov/hmrc/trackingconsentfrontend/assets/src/interfaces/optimizelyCommunicatorFactory.ts
@@ -5,13 +5,37 @@ const optimizelyCommunicatorFactory = (window: Window): Communicator => ({
   sendPreferences: (userPreferences: UserPreferences) => {
 
     const setOptimizelyOptOut = (isOptOut: Boolean) => {
-      window.optimizely.push({ 'type': 'optOut', isOptOut });
+      window.optimizely.push({ 'type': 'optOut', isOptOut })
     }
 
-    window.optimizely = window.optimizely || [];
+    const setupOptimizelyIfConfigured = () => {
+      const regex = /^(\/|(https:)?\/\/www\.tax\.service\.gov\.uk\/|(https:)?\/\/www\.(qa|dev|staging)\.tax\.service\.gov\.uk\/|(http:)?\/\/localhost:12345\/)tracking-consent/
+      const isTruthy = x => !!x
+      const scriptTags = Array.from(document.getElementsByTagName('script'))
+      const trackingConsentScripts = scriptTags
+        .filter(scriptTag => (scriptTag.getAttribute('src') || '')
+          .match(regex))
+      const optimizelyIDs = trackingConsentScripts
+        .map(scriptTag => scriptTag.getAttribute('data-optimizely-id'))
+        .filter(isTruthy)
+      const firstTrackingConsentScriptTag = trackingConsentScripts[0]
+
+      optimizelyIDs.forEach(optimizelyId => {
+        if (!optimizelyId || !optimizelyId.match(/^\d{11}$/)) {
+          console.error('Optimizely ID is malformed', optimizelyId)
+        } else {
+          const script = document.createElement('script')
+          script.setAttribute('src', `https://cdn.optimizely.com/js/${optimizelyId}.js`)
+          firstTrackingConsentScriptTag.after(script)
+        }
+      })
+    }
+
+    window.optimizely = window.optimizely || []
     const preferences = userPreferences.getPreferences() || {}
     if (preferences.measurement === true) {
       setOptimizelyOptOut(false)
+      setupOptimizelyIfConfigured()
     } else {
       setOptimizelyOptOut(true)
     }
--- app/uk/gov/hmrc/trackingconsentfrontend/assets/test/interfaces/optimizelyCommunicatorFactory.spec.ts
+++ app/uk/gov/hmrc/trackingconsentfrontend/assets/test/interfaces/optimizelyCommunicatorFactory.spec.ts
@@ -2,6 +2,43 @@ import optimizelyCommunicatorFactory from '../../src/interfaces/optimizelyCommun
 
 describe('sendPreferences', () => {
   let testScope
+
+  const getScripts = () => Array.from(document.getElementsByTagName('script'))
+  const getScriptSrcs = () => getScripts().map(e => e.getAttribute('src'))
+  const exampleTCScriptURL = '/tracking-consent/tracking.js'
+
+  const setupScriptWithID = (id, scriptSrc: string) => {
+    const originalScripts = getScripts()
+    const head = document.getElementsByTagName('head')[0]
+    const scriptTagWithAttr = document.createElement('script')
+    if (scriptSrc) {
+      scriptTagWithAttr.setAttribute('src', scriptSrc)
+    }
+    if (id) {
+      scriptTagWithAttr.setAttribute('data-optimizely-id', id)
+    }
+    head.appendChild(scriptTagWithAttr)
+
+    return {
+      trigger: () => {
+        testScope.userPreferences.getPreferences.mockReturnValue({
+          measurement: true,
+          marketing: false,
+          settings: false
+        })
+        testScope.preferenceCommunicator.sendPreferences(testScope.userPreferences)
+      },
+      cleanup: () => {
+        getScripts()
+          .forEach(script => {
+            if (!originalScripts.includes(script)) {
+              (script.parentNode || document.createElement('div')).removeChild(script)
+            }
+          })
+      }
+    }
+  }
+
   beforeEach(() => {
     testScope = { window: {} }
     testScope.preferenceCommunicator = optimizelyCommunicatorFactory(testScope.window)
@@ -11,6 +48,7 @@ describe('sendPreferences', () => {
       marketing: false,
       settings: false
     })
+    jest.spyOn(console, 'error')
   })
 
   it('should set optOut to true when measurement is set to false', () => {
@@ -67,5 +105,86 @@ describe('sendPreferences', () => {
     expect(testScope.window.optimizely).toEqual([
       { type: 'optOut', isOptOut: true }
     ])
+  })
+
+  it('should setup optimizely when configured', () => {
+    const control = setupScriptWithID('12345678901', exampleTCScriptURL)
+    control.trigger()
+
+    expect(getScriptSrcs())
+      .toEqual([
+        exampleTCScriptURL,
+        'https://cdn.optimizely.com/js/12345678901.js'
+      ])
+    expect(console.error).not.toHaveBeenCalled()
+
+    control.cleanup()
+  })
+
+  it('should setup optimizely when configured', () => {
+    const control = setupScriptWithID('98765432109', exampleTCScriptURL)
+    control.trigger()
+
+    expect(getScriptSrcs())
+      .toEqual([
+        exampleTCScriptURL,
+        'https://cdn.optimizely.com/js/98765432109.js'
+      ])
+    expect(console.error).not.toHaveBeenCalled()
+
+    control.cleanup()
+  })
+
+  it('should not setup optimizely when configured on the wrong script', () => {
+    const nonTrackingConsentScriptSrc = 'https://code.jquery.com/jquery-3.5.1.min.js'
+    const control = setupScriptWithID('98765432109', nonTrackingConsentScriptSrc)
+    control.trigger()
+
+    expect(getScriptSrcs())
+      .toEqual([
+        nonTrackingConsentScriptSrc
+      ])
+    expect(console.error).not.toHaveBeenCalled()
+
+    control.cleanup()
+  })
+
+  it('should not setup optimizely when configured on an inline script', () => {
+    const control = setupScriptWithID('98765432109', '')
+    control.trigger()
+
+    expect(getScriptSrcs())
+      .toEqual([
+        null // the inline script
+      ])
+    expect(console.error).not.toHaveBeenCalled()
+
+    control.cleanup()
+  })
+
+  it('should not setup optimizely when no id is present', () => {
+    const control = setupScriptWithID(null, exampleTCScriptURL)
+    control.trigger()
+
+    expect(getScriptSrcs())
+      .toEqual([
+        exampleTCScriptURL
+      ])
+    expect(console.error).not.toHaveBeenCalled()
+
+    control.cleanup()
+  })
+
+  it('should fail when invalid ID is passed', () => {
+    const control = setupScriptWithID('a8765432109', exampleTCScriptURL)
+    control.trigger()
+
+    expect(console.error).toHaveBeenCalledWith('Optimizely ID is malformed', 'a8765432109')
+    expect(getScriptSrcs())
+      .toEqual([
+        exampleTCScriptURL
+      ])
+
+    control.cleanup()
   })
 })
