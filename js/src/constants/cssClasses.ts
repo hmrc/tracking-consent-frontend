12a3fbccf9765192aa9533b95796a693fcb2aa1b
Nat Carey <nat@dorightdigital.com>
2021-06-18 13:45:28 +0100

    Adopting GDS confirmation.

--- js/src/constants/cssClasses.ts
+++ js/src/constants/cssClasses.ts
@@ -6,12 +6,13 @@ export const COOKIE_BANNER_GOV_UK_WIDTH_CONTAINER_CLASS = 'cbanner-govuk-width-c
 
 // Cookie settings related (uses standard Gov UK Frontend)
 export const COOKIE_SETTINGS_NOTICE_CLASS = 'cookie-settings__notice';
+export const COOKIE_SETTINGS_NOTICE_CONTENT_CLASS = 'cookie-settings__notice-content';
 export const COOKIE_SETTINGS_CONFIRMATION_CLASS = 'cookie-settings__confirmation';
 export const COOKIE_SETTINGS_NOTICE_WRAPPER_CLASS = 'cookie-settings__notice-wrapper';
 
 // Gov UK frontend
 export const GOV_UK_BODY_CLASS = 'govuk-body';
-export const GOV_UK_LINK_CLASS = 'govuk-link';
+export const GOV_UK_NOTIFICATION_LINK_CLASS = 'govuk-notification-banner__link';
 export const GOV_UK_SKIP_LINK_CLASS = 'govuk-skip-link';
 export const SKIP_LINK_CONTAINER_ID = 'skiplink-container';
 
--- js/src/ui/cookieSettingsConfirmation.ts
+++ js/src/ui/cookieSettingsConfirmation.ts
@@ -1,5 +1,11 @@
 const cookieSettingsConfirmation = (messages) => `
-<section class="cookie-settings__notice" aria-label="Notice" aria-live="polite" role="region" tabindex="-1">
+<section class="cookie-settings__notice govuk-notification-banner govuk-notification-banner--success" role="alert" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner" tabindex="-1">
+  <div class="govuk-notification-banner__header">
+    <h1 class="govuk-notification-banner__title" id="govuk-notification-banner-title">
+      Success
+    </h1>
+  </div>
+  <div class="govuk-notification-banner__content cookie-settings__notice-content">
     <h2 class="govuk-heading-m">${messages['cookieSettings.saveConfirmation.title']}</h2>
     <p class="govuk-body">${messages['cookieSettings.saveConfirmation.paragraph1']}</p>
 </section>
