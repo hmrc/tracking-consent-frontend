f1e519979c16f591c1cdfad19f44425fd6d7b3f7
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2021-10-07 16:01:28 +0100

    Update AppDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,21 +1,20 @@
-import play.core.PlayVersion.current
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.6.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "1.8.0-play-28"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.14.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "1.19.0-play-28"
   )
 
   val test = Seq(
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.14.0"  % "test",
+    "uk.gov.hmrc"            %% "webdriver-factory"      % "0.23.0"  % "test",
     "org.jsoup"               % "jsoup"                  % "1.10.2"  % "test",
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.6.0"   % "test",
     "com.vladsch.flexmark"    % "flexmark-all"           % "0.35.10" % "test",
     "org.scalatest"          %% "scalatest"              % "3.2.9"   % "test",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"   % "test",
     "org.scalatestplus"      %% "selenium-3-141"         % "3.2.9.0" % "test",
-    "uk.gov.hmrc"            %% "webdriver-factory"      % "0.22.0"  % "test",
     "com.typesafe"            % "config"                 % "1.3.2"   % "test",
     "com.github.tomakehurst"  % "wiremock-jre8"          % "2.21.0"  % "test"
   )
