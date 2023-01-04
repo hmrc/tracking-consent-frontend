414671d78a718864658a3190c66e1cdc92ff9ee3
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-01-04 15:06:21 +0000

    NOJIRA: Bumping sbt version and app dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,4 +1,3 @@
-import play.core.PlayVersion.current
 import sbt._
 
 object AppDependencies {
@@ -7,7 +6,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.24.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "5.5.0-play-28"
   )
 
   val test = Seq(
@@ -17,7 +16,7 @@ object AppDependencies {
     "org.scalatest"          %% "scalatest"              % "3.2.13"         % "test",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"          % "test",
     "org.scalatestplus"      %% "selenium-4-2"           % "3.2.13.0"       % "test",
-    "uk.gov.hmrc"            %% "webdriver-factory"      % "0.38.0"         % "test",
+    "uk.gov.hmrc"            %% "webdriver-factory"      % "0.41.0"         % "test",
     "com.typesafe"            % "config"                 % "1.4.2"          % "test",
     "com.github.tomakehurst"  % "wiremock-jre8"          % "2.27.2"         % "test"
   )
