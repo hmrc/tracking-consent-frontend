5f798658a1035de76617c12a2a7b1d9cc2fa13a0
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-03-20 14:04:00 +0000

    PLATUI-2249: Updated service dependencies prior to CSP testing

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "6.5.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.1.0-play-28"
   )
 
   val test = Seq(
