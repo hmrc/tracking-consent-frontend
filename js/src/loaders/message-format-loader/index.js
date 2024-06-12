a5690708b9cf955677f577868846b40d6a3d8a55
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-06-12 16:14:06 +0100

    PLATUI-3064: Rolling back certain code changes that turned out not needed

--- js/src/loaders/message-format-loader/index.js
+++ js/src/loaders/message-format-loader/index.js
@@ -15,9 +15,11 @@ const process = (content) => {
 
   const strippedMessages = stripDoubleQuotesFromMessages(messages);
 
-  return {
-    code: `module.exports = ${JSON.stringify(strippedMessages, null, 2)}`
-  }
+  // return {
+  //   code: `module.exports = ${JSON.stringify(strippedMessages, null, 2)}`
+  // }
+  return `module.exports = ${JSON.stringify(strippedMessages, null, 2)}`;
+
 };
 
 module.exports = process;
--- js/test/loaders/messageFormatLoader.spec.ts
+++ js/test/loaders/messageFormatLoader.spec.ts
@@ -9,14 +9,18 @@ banner.paragraph1.link.text = cookies to collect information
   it('should translate a messages file', () => {
     const output = loader(messages);
 
-    expect(output).toEqual({"code": `module.exports = {\n  \"banner.title\": \"Tell us whether you accept cookies\",\n  \"banner.paragraph1.beforeLink\": \"We use\",\n  \"banner.paragraph1.link.text\": \"cookies to collect information\"\n}`});
+    expect(output).toEqual(`module.exports = {
+  "banner.title": "Tell us whether you accept cookies",
+  "banner.paragraph1.beforeLink": "We use",
+  "banner.paragraph1.link.text": "cookies to collect information"
+}`);
   });
 
   it('should interpret double quotes as single quotes', () => {
     const output = loader('banner.saveConfirmation.paragraph1.beforeLink = You\'\'ve accepted all cookies. You can ');
 
-    expect(output).toEqual({
-      code: `module.exports = {\n  "banner.saveConfirmation.paragraph1.beforeLink": "You've accepted all cookies. You can "\n}`
-    });
+    expect(output).toEqual(`module.exports = {
+  "banner.saveConfirmation.paragraph1.beforeLink": "You've accepted all cookies. You can "
+}`);
   });
 });
