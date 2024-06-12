d314a0ab49d7d9cd880e74a9218a0cd672e0cc6a
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-06-12 14:50:25 +0100

    PLATUI-3064: Updating tests to new Jest format

--- js/test/messageFormatJestTransformer.js
+++ js/test/messageFormatJestTransformer.js
@@ -1,13 +1,5 @@
 const loader = require('../src/loaders/message-format-loader');
 
-// module.exports = {
-//   process: loader,
-// };
-
 module.exports = {
-  process(sourceText, sourcePath, options) {
-    return {
-      code: () => loader(),
-    };
-  }
-}
+  process: loader,
+};
