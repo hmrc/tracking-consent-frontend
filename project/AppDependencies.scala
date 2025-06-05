6e7aa1ecea3892811d75b13acd2d77dac48ba8df
Patryk Rudnicki <23506899+PatrykRudnicki@users.noreply.github.com>
2025-06-05 10:23:56 +0100

    PLATUI-3749 Bump ui-test-runner

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,6 +15,6 @@ object AppDependencies {
     "org.jsoup"          % "jsoup"                        % "1.18.3"         % Test,
     "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion" % bootstrapVersion % Test,
     "org.scalatestplus" %% "selenium-4-21"                % "3.2.19.0"       % Test,
-    "uk.gov.hmrc"       %% "ui-test-runner"               % "0.45.0"         % Test
+    "uk.gov.hmrc"       %% "ui-test-runner"               % "0.46.0"         % Test
   )
 }
