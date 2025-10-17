b1d419dd1f1dc0454da9a33b454c430164e16f70
Joanna Pinto Paul <joanna.pinto.paul@digital.hmrc.gov.uk>
2025-10-17 16:53:28 +0100

    PLATUI-4000: Uplifted dependencies (#335)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
-  private val frontendVersion  = "12.15.0"
+  private val bootstrapVersion = "10.3.0"
+  private val frontendVersion  = "12.18.0"
   private val playVersion      = "play-30"
 
   val compile = Seq(
@@ -15,6 +15,6 @@ object AppDependencies {
     "org.jsoup"          % "jsoup"                        % "1.18.3"         % Test,
     "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion" % bootstrapVersion % Test,
     "org.scalatestplus" %% "selenium-4-21"                % "3.2.19.0"       % Test,
-    "uk.gov.hmrc"       %% "ui-test-runner"               % "0.49.0"         % Test
+    "uk.gov.hmrc"       %% "ui-test-runner"               % "0.50.0"         % Test
   )
 }
