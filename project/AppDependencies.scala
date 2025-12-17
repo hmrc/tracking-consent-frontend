bf9b90f9964975fd11e3559fc2df9325452358b8
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2025-12-17 16:00:15 +0000

    NOJIRA cleanup dependencies and bump version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.4.0"
-  private val frontendVersion  = "12.22.0"
+  private val frontendVersion  = "12.23.0"
   private val playVersion      = "play-30"
 
   val compile = Seq(
@@ -15,6 +15,6 @@ object AppDependencies {
     "org.jsoup"          % "jsoup"                        % "1.18.3"         % Test,
     "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion" % bootstrapVersion % Test,
     "org.scalatestplus" %% "selenium-4-21"                % "3.2.19.0"       % Test,
-    "uk.gov.hmrc"       %% "ui-test-runner"               % "0.50.0"         % Test
+    "uk.gov.hmrc"       %% "ui-test-runner"               % "0.52.0"         % Test
   )
 }
