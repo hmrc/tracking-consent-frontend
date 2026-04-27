4230eb740bb9b5e6ee0e025ef60626e7f83caa6b
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2026-04-27 13:34:55 +0100

    PLATUI-4263: tweak typescript

--- js/src/interfaces/withUseServiceNavigationQueryParam.ts
+++ js/src/interfaces/withUseServiceNavigationQueryParam.ts
@@ -1,15 +1,17 @@
 import getCurrentScript from '../common/getCurrentScript';
 
-function currentPageIsUsingServiceNavigation() {
-  return getCurrentScript()?.hasAttribute('data-use-service-nav')
-        || document.querySelector('.govuk-service-navigation');
+function currentPageIsUsingServiceNavigation(): boolean {
+  return Boolean(
+    getCurrentScript()?.hasAttribute('data-use-service-nav')
+      || document.querySelector('.govuk-service-navigation'),
+  );
 }
 
-const withUseServiceNavigationQueryParam = (url: String) => {
+function withUseServiceNavigationQueryParam(url: string): string {
   if (currentPageIsUsingServiceNavigation()) {
     return `${url}?useServiceNavigation`; // all we need for our narrow use case
   }
   return url;
-};
+}
 
 export default withUseServiceNavigationQueryParam;
